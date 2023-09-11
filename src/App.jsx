6af4570f729edc7943d179a5e5b65976f43a5f27
Kevin Peset <kevin.peset@gmail.com>
2023-09-11 11:41:56 +0200

    Ajout navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,57 @@
-import axios from "axios"
-import { useState } from "react"
-import CharacterCard from "./components/CharacterCard"
+import axios from "axios";
+import { useState } from "react";
+import CharacterCard from "./components/CharacterCard";
+import Header from "./components/Header";
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const getCharacters = (event) => {
-
-    const house = event.target.name
+    const house = event.target.name;
 
     axios
       .get(`https://hp-api.onrender.com/api/characters/house/${house}`)
       .then((response) => {
-        setData(response.data)
-      })
-  }
+        setData(response.data);
+      });
+  };
 
   return (
     <>
-      <header className="homepage_header">
-        <h1>Poudlard Papote</h1>
-      </header>
+      <Header />
       <section className="homepage_content">
         <div className="panel">
           <h2>Maisons de Poudlard</h2>
-          <p>Cliquez sur le bouton pour afficher les élèves de la maison correspondante.</p>
+          <p>
+            Cliquez sur le bouton pour afficher les élèves de la maison
+            correspondante.
+          </p>
           <div className="nav_houses">
-            <button name="gryffindor" onClick={getCharacters}>GRYFFONDOR</button>
-            <button name="hufflepuff" onClick={getCharacters}>POUFSOUFFLE</button>
-            <button name="ravenclaw" onClick={getCharacters}>SERDAIGLE</button>
-            <button name="slytherin" onClick={getCharacters}>SERPENTARD</button>
+            <button name="gryffindor" onClick={getCharacters}>
+              GRYFFONDOR
+            </button>
+            <button name="hufflepuff" onClick={getCharacters}>
+              POUFSOUFFLE
+            </button>
+            <button name="ravenclaw" onClick={getCharacters}>
+              SERDAIGLE
+            </button>
+            <button name="slytherin" onClick={getCharacters}>
+              SERPENTARD
+            </button>
           </div>
         </div>
         <div className="cards">
-          {data.map((character) => character.image && <CharacterCard key={character.id} character={character} />)}
+          {data.map(
+            (character) =>
+              character.image && (
+                <CharacterCard key={character.id} character={character} />
+              )
+          )}
         </div>
       </section>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className="homepage_header">
+      <h1>Poudlard Papote</h1>
+      <nav>
+        <div className="line_nav" />
+        <ul>
+          <li>Accueil</li>
+          <li>Maisons</li>
+          <li>Contact</li>
+        </ul>
+        <div className="line_nav" />
+      </nav>
+    </header>
+  );
+}
